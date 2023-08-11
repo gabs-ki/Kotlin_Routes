69ddd50d7b6eeca800821b166dcd66ed8b0f7996
MARCELO GABRIEL VIEIRA SANTIAGO <22282204@dsn127m1249386.cfp127aluno.local>
2023-08-11 13:35:34 -0300

    Adicionando Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,10 @@ dependencies {
     // Dependencia do Navigation
     implementation("androidx.navigation:navigation-compose:2.6.0")
 
+    // Dependecia do Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     // Animacao na Navegacao
     implementation("com.google.accompanist:accompanist-navigation-animation:0.30.1")
 }
